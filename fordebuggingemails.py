54ba950fd55d6c4082cfde20028de3a081ca4c11
Jagadish Mohanty <70753983+JagadishMohanty2@users.noreply.github.com>
2020-09-15 13:32:44 +0200

    Add files via upload

---
+++ fordebuggingemails.py
@@ -0,0 +1,33 @@
+# this code is only applicable for debugging
+
+import smtplib # importing smtp
+
+"""
+go to the terminal and write: (for making a debugging server)
+python3 -m smtpd -c DebuggingServer -n localhost:1025, 1025 is the port number
+
+if it says python3 not recognize then replace python3 with python and run it again
+"""
+
+from email.message import EmailMessage
+import imghdr
+
+msg = EmailMessage()
+msg['Subject'] = 'Python.org email' # that is the topic
+msg['From'] = 'your_email@gmailmail.com' # your email
+msg['To'] = 'receivers_email@gmailmail.com' # receivers email
+
+msg.set_content('This was sent using python') # this is the body
+
+# to attach files
+with open('senditemail.py','r') as f:
+	file_data = f.read() # getting data
+	file_type = imghdr.what(f.name) # getting the extension
+	file_name = f.name # getting the file name
+
+msg.add_attachment(file_data, filename=file_name)
+
+# only applicable for debugging purposes, enter the port number which u chose
+with smtplib.SMTP('localhost', port=1025) as smtp:		
+	smtp.send_message(msg) # sending the message 
+
